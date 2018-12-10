00f3fcc445d6151605598decc9877f488ee8f2e5
Ichinose Shogo <shogo82148@gmail.com>
2018-12-10 09:13:42 +0900

    make zip file

--- author/build-perl-layer.sh
+++ author/build-perl-layer.sh
@@ -2,9 +2,12 @@
 
 ROOT=$(cd "$(dirname "$0")/../" && pwd)
 PERL_VERSION=$1
+TAG=$2
 OPT="$ROOT/.perl-layer/$PERL_VERSION"
 set -ue
 
 rm -rf "$OPT"
 mkdir -p "$OPT"
 docker run -v "$ROOT:/var/task" -v "$OPT:/opt" --rm lambci/lambda:build-provided ./author/build-perl.sh "$PERL_VERSION"
+cd "$OPT"
+zip -r "$ROOT/lambda-perl-layer-$TAG.zip" .
