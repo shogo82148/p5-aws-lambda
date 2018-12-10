f3bae13278038ebcf2a34890d647cfbbc93270a6
Ichinose Shogo <shogo82148@gmail.com>
2018-12-10 20:38:52 +0900

    clean up before build the layer.

--- author/build-perl-layer.sh
+++ author/build-perl-layer.sh
@@ -6,8 +6,11 @@ TAG=$2
 OPT="$ROOT/.perl-layer/$PERL_VERSION"
 set -ue
 
+# clean up
 rm -rf "$OPT"
 mkdir -p "$OPT"
+rm -f "$ROOT/lambda-perl-layer-$TAG.zip"
+
 docker run -v "$ROOT:/var/task" -v "$OPT:/opt" --rm lambci/lambda:build-provided ./author/build-perl.sh "$PERL_VERSION"
 cd "$OPT"
-zip -r "$ROOT/lambda-perl-layer-$TAG.zip" .
+zip -9 -r "$ROOT/lambda-perl-layer-$TAG.zip" .
