1181dfe86d88a6d0c652b4dbdff9d2bcc73949d7
Ichinose Shogo <shogo82148@gmail.com>
2019-04-17 11:06:49 +0900

    fix script path

--- author/build-perl.sh
+++ author/build-perl.sh
@@ -6,4 +6,4 @@ curl -sL https://raw.githubusercontent.com/tokuhirom/Perl-Build/master/perl-buil
 perl /tmp/perl-build "$PERL_VERSION" /opt/
 curl -sL https://cpanmin.us | /opt/bin/perl - App::cpanminus
 /opt/bin/cpanm -n .
-cp bin/bootstrap /opt/
+cp script/bootstrap /opt/
