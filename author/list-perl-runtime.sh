517b1be81906905efbccaef80223d6debd634695
Ichinose Shogo <shogo82148@gmail.com>
2019-07-17 01:38:54 +0900

    format arn list as pod

--- author/list-perl-runtime.sh
+++ author/list-perl-runtime.sh
@@ -2,26 +2,42 @@
 
 ROOT=$(cd "$(dirname "$0")/../" && pwd)
 
-set -ue
-
 "$ROOT/author/validate-account.sh" || exit 2
 
-for ZIP in "$ROOT"/.perl-layer/dist/perl-*-runtime.zip; do
+set -ue
+
+for ZIP in $(find "$ROOT"/.perl-layer/dist -name perl-\*-runtime.zip | sort -r); do
     NAME=$(basename "$ZIP" .zip)
     PERLVERION=$(echo "$NAME" | cut -d- -f2,3 | sed -e 's/-/./')
     STACK="${PERLVERION//./-}"
+    echo "=item Perl $PERLVERION"
+    echo
+    echo "=over"
+    echo
     while read -r REGION; do
-        aws --region "$REGION" cloudformation describe-stacks \
-            --stack-name "lambda-$STACK-runtime" | jq -r .Stacks[0].Outputs[0].OutputValue
+        ARN=$(aws --region "$REGION" cloudformation describe-stacks \
+            --stack-name "lambda-$STACK-runtime" | jq -r .Stacks[0].Outputs[0].OutputValue)
+        echo "=item C<$ARN>"
+        echo
     done < "$ROOT/author/regions.txt"
+    echo "=back"
+    echo
 done
 
-for ZIP in "$ROOT"/.perl-layer/dist/perl-*-paws.zip; do
+for ZIP in $(find "$ROOT"/.perl-layer/dist -name perl-\*-paws.zip | sort -r); do
     NAME=$(basename "$ZIP" .zip)
     PERLVERION=$(echo "$NAME" | cut -d- -f2,3 | sed -e 's/-/./')
     STACK="${PERLVERION//./-}"
+    echo "=item Perl $PERLVERION"
+    echo
+    echo "=over"
+    echo
     while read -r REGION; do
-        aws --region "$REGION" cloudformation describe-stacks \
-            --stack-name "lambda-$STACK-paws" | jq -r .Stacks[0].Outputs[0].OutputValue
+        ARN=$(aws --region "$REGION" cloudformation describe-stacks \
+            --stack-name "lambda-$STACK-paws" | jq -r .Stacks[0].Outputs[0].OutputValue)
+        echo "=item C<$ARN>"
+        echo
     done < "$ROOT/author/regions.txt"
+    echo "=back"
+    echo
 done
