7c29f05057c105e02bd29d0be3f68578dafbc34f
Ichinose Shogo <shogo82148@gmail.com>
2019-07-17 00:20:50 +0900

    list paws layers

--- author/list-perl-runtime.sh
+++ author/list-perl-runtime.sh
@@ -6,12 +6,22 @@ set -ue
 
 "$ROOT/author/validate-account.sh" || exit 2
 
-for ZIP in "$ROOT"/.perl-layer/dist/*.zip; do
+for ZIP in "$ROOT"/.perl-layer/dist/perl-*-runtime.zip; do
     NAME=$(basename "$ZIP" .zip)
     PERLVERION=$(echo "$NAME" | cut -d- -f2,3 | sed -e 's/-/./')
     STACK="${PERLVERION//./-}"
     while read -r REGION; do
         aws --region "$REGION" cloudformation describe-stacks \
             --stack-name "lambda-$STACK-runtime" | jq -r .Stacks[0].Outputs[0].OutputValue
+    done < "$ROOT/author/regions.txt"
+done
+
+for ZIP in "$ROOT"/.perl-layer/dist/perl-*-paws.zip; do
+    NAME=$(basename "$ZIP" .zip)
+    PERLVERION=$(echo "$NAME" | cut -d- -f2,3 | sed -e 's/-/./')
+    STACK="${PERLVERION//./-}"
+    while read -r REGION; do
+        aws --region "$REGION" cloudformation describe-stacks \
+            --stack-name "lambda-$STACK-paws" | jq -r .Stacks[0].Outputs[0].OutputValue
     done < "$ROOT/author/regions.txt"
 done
