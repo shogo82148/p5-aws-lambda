6331e9d47c8c15a438f5e241e85a968984b5cc93
Ichinose Shogo <shogo82148@gmail.com>
2019-08-17 16:15:26 +0900

    add AWS::Lambda::context

--- t/test_handlers/echo.pl
+++ t/test_handlers/echo.pl
@@ -1,9 +1,13 @@
 use utf8;
 use strict;
 use warnings;
+use AWS::Lambda;
 
 sub handle {
-    my $payload = shift;
+    my ($payload, $context) = @_;
+    die "payload is empty" unless $payload;
+    die "context is empty" unless $context;
+    die "AWS::Lambda::context is invalid" if $AWS::Lambda::context != $context;
     return $payload;
 }
 
