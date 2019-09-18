2bd5476ff63aa19cd6792fd48bb9413f8ba76ebc
Ichinose Shogo <shogo82148@gmail.com>
2019-09-18 20:30:28 +0900

    use GNU Parallel

--- author/build-paws.sh
+++ author/build-paws.sh
@@ -9,13 +9,10 @@ unzip "/var/task/.perl-layer/dist/perl-$TAG-runtime.zip"
 
 # install perlstrip
 # https://metacpan.org/pod/distribution/Perl-Strip/bin/perlstrip
+yum install -y parallel
 cpan -T Perl::Strip
 
 set +e # skip errors of stripping
 
-find /opt/lib/perl5/site_perl -type f -a -name '*.pm' | while read -r MODULE; do
-    perlstrip -v -o "$MODULE.strip" "$MODULE" \
-        && /opt/bin/perl -c "$MODULE.strip" \
-        && mv "$MODULE.strip" "$MODULE"
-    rm -f "$MODULE.strip"
-done
+find /opt/lib/perl5/site_perl -type f -a -name '*.pm' -print0 | parallel -0 perlstrip -v
+find /opt/lib/perl5/site_perl -type f -a -name '*.pod' -print0 | xargs -0 rm
