8c5642b0547fa9c9b540b378a95d8f4d54cafcb4
Ichinose Shogo <shogo82148@gmail.com>
2019-11-13 11:49:47 +0900

    update arn list

--- lib/AWS/Lambda.pm
+++ lib/AWS/Lambda.pm
@@ -95,39 +95,39 @@ The Layer ARN list is here.
 
 =over
 
-=item C<arn:aws:lambda:ap-east-1:445285296882:layer:perl-5-30-runtime:4>
+=item C<arn:aws:lambda:ap-east-1:445285296882:layer:perl-5-30-runtime:5>
 
-=item C<arn:aws:lambda:ap-northeast-1:445285296882:layer:perl-5-30-runtime:4>
+=item C<arn:aws:lambda:ap-northeast-1:445285296882:layer:perl-5-30-runtime:5>
 
-=item C<arn:aws:lambda:ap-northeast-2:445285296882:layer:perl-5-30-runtime:4>
+=item C<arn:aws:lambda:ap-northeast-2:445285296882:layer:perl-5-30-runtime:5>
 
-=item C<arn:aws:lambda:ap-south-1:445285296882:layer:perl-5-30-runtime:4>
+=item C<arn:aws:lambda:ap-south-1:445285296882:layer:perl-5-30-runtime:5>
 
-=item C<arn:aws:lambda:ap-southeast-1:445285296882:layer:perl-5-30-runtime:4>
+=item C<arn:aws:lambda:ap-southeast-1:445285296882:layer:perl-5-30-runtime:5>
 
-=item C<arn:aws:lambda:ap-southeast-2:445285296882:layer:perl-5-30-runtime:4>
+=item C<arn:aws:lambda:ap-southeast-2:445285296882:layer:perl-5-30-runtime:5>
 
-=item C<arn:aws:lambda:ca-central-1:445285296882:layer:perl-5-30-runtime:4>
+=item C<arn:aws:lambda:ca-central-1:445285296882:layer:perl-5-30-runtime:5>
 
-=item C<arn:aws:lambda:eu-central-1:445285296882:layer:perl-5-30-runtime:4>
+=item C<arn:aws:lambda:eu-central-1:445285296882:layer:perl-5-30-runtime:5>
 
-=item C<arn:aws:lambda:eu-west-1:445285296882:layer:perl-5-30-runtime:4>
+=item C<arn:aws:lambda:eu-west-1:445285296882:layer:perl-5-30-runtime:5>
 
-=item C<arn:aws:lambda:eu-west-2:445285296882:layer:perl-5-30-runtime:4>
+=item C<arn:aws:lambda:eu-west-2:445285296882:layer:perl-5-30-runtime:5>
 
-=item C<arn:aws:lambda:eu-west-3:445285296882:layer:perl-5-30-runtime:4>
+=item C<arn:aws:lambda:eu-west-3:445285296882:layer:perl-5-30-runtime:5>
 
-=item C<arn:aws:lambda:me-south-1:445285296882:layer:perl-5-30-runtime:2>
+=item C<arn:aws:lambda:me-south-1:445285296882:layer:perl-5-30-runtime:3>
 
-=item C<arn:aws:lambda:sa-east-1:445285296882:layer:perl-5-30-runtime:4>
+=item C<arn:aws:lambda:sa-east-1:445285296882:layer:perl-5-30-runtime:5>
 
-=item C<arn:aws:lambda:us-east-1:445285296882:layer:perl-5-30-runtime:4>
+=item C<arn:aws:lambda:us-east-1:445285296882:layer:perl-5-30-runtime:5>
 
-=item C<arn:aws:lambda:us-east-2:445285296882:layer:perl-5-30-runtime:4>
+=item C<arn:aws:lambda:us-east-2:445285296882:layer:perl-5-30-runtime:5>
 
-=item C<arn:aws:lambda:us-west-1:445285296882:layer:perl-5-30-runtime:4>
+=item C<arn:aws:lambda:us-west-1:445285296882:layer:perl-5-30-runtime:5>
 
-=item C<arn:aws:lambda:us-west-2:445285296882:layer:perl-5-30-runtime:4>
+=item C<arn:aws:lambda:us-west-2:445285296882:layer:perl-5-30-runtime:5>
 
 =back
 
@@ -135,39 +135,39 @@ The Layer ARN list is here.
 
 =over
 
-=item C<arn:aws:lambda:ap-east-1:445285296882:layer:perl-5-28-runtime:4>
+=item C<arn:aws:lambda:ap-east-1:445285296882:layer:perl-5-28-runtime:5>
 
-=item C<arn:aws:lambda:ap-northeast-1:445285296882:layer:perl-5-28-runtime:11>
+=item C<arn:aws:lambda:ap-northeast-1:445285296882:layer:perl-5-28-runtime:12>
 
-=item C<arn:aws:lambda:ap-northeast-2:445285296882:layer:perl-5-28-runtime:11>
+=item C<arn:aws:lambda:ap-northeast-2:445285296882:layer:perl-5-28-runtime:12>
 
-=item C<arn:aws:lambda:ap-south-1:445285296882:layer:perl-5-28-runtime:11>
+=item C<arn:aws:lambda:ap-south-1:445285296882:layer:perl-5-28-runtime:12>
 
-=item C<arn:aws:lambda:ap-southeast-1:445285296882:layer:perl-5-28-runtime:11>
+=item C<arn:aws:lambda:ap-southeast-1:445285296882:layer:perl-5-28-runtime:12>
 
-=item C<arn:aws:lambda:ap-southeast-2:445285296882:layer:perl-5-28-runtime:11>
+=item C<arn:aws:lambda:ap-southeast-2:445285296882:layer:perl-5-28-runtime:12>
 
-=item C<arn:aws:lambda:ca-central-1:445285296882:layer:perl-5-28-runtime:11>
+=item C<arn:aws:lambda:ca-central-1:445285296882:layer:perl-5-28-runtime:12>
 
-=item C<arn:aws:lambda:eu-central-1:445285296882:layer:perl-5-28-runtime:11>
+=item C<arn:aws:lambda:eu-central-1:445285296882:layer:perl-5-28-runtime:12>
 
-=item C<arn:aws:lambda:eu-west-1:445285296882:layer:perl-5-28-runtime:11>
+=item C<arn:aws:lambda:eu-west-1:445285296882:layer:perl-5-28-runtime:12>
 
-=item C<arn:aws:lambda:eu-west-2:445285296882:layer:perl-5-28-runtime:11>
+=item C<arn:aws:lambda:eu-west-2:445285296882:layer:perl-5-28-runtime:12>
 
-=item C<arn:aws:lambda:eu-west-3:445285296882:layer:perl-5-28-runtime:11>
+=item C<arn:aws:lambda:eu-west-3:445285296882:layer:perl-5-28-runtime:12>
 
-=item C<arn:aws:lambda:me-south-1:445285296882:layer:perl-5-28-runtime:2>
+=item C<arn:aws:lambda:me-south-1:445285296882:layer:perl-5-28-runtime:3>
 
-=item C<arn:aws:lambda:sa-east-1:445285296882:layer:perl-5-28-runtime:11>
+=item C<arn:aws:lambda:sa-east-1:445285296882:layer:perl-5-28-runtime:12>
 
-=item C<arn:aws:lambda:us-east-1:445285296882:layer:perl-5-28-runtime:11>
+=item C<arn:aws:lambda:us-east-1:445285296882:layer:perl-5-28-runtime:12>
 
-=item C<arn:aws:lambda:us-east-2:445285296882:layer:perl-5-28-runtime:11>
+=item C<arn:aws:lambda:us-east-2:445285296882:layer:perl-5-28-runtime:12>
 
-=item C<arn:aws:lambda:us-west-1:445285296882:layer:perl-5-28-runtime:11>
+=item C<arn:aws:lambda:us-west-1:445285296882:layer:perl-5-28-runtime:12>
 
-=item C<arn:aws:lambda:us-west-2:445285296882:layer:perl-5-28-runtime:11>
+=item C<arn:aws:lambda:us-west-2:445285296882:layer:perl-5-28-runtime:12>
 
 =back
 
@@ -175,39 +175,39 @@ The Layer ARN list is here.
 
 =over
 
-=item C<arn:aws:lambda:ap-east-1:445285296882:layer:perl-5-26-runtime:5>
+=item C<arn:aws:lambda:ap-east-1:445285296882:layer:perl-5-26-runtime:6>
 
-=item C<arn:aws:lambda:ap-northeast-1:445285296882:layer:perl-5-26-runtime:12>
+=item C<arn:aws:lambda:ap-northeast-1:445285296882:layer:perl-5-26-runtime:13>
 
-=item C<arn:aws:lambda:ap-northeast-2:445285296882:layer:perl-5-26-runtime:12>
+=item C<arn:aws:lambda:ap-northeast-2:445285296882:layer:perl-5-26-runtime:13>
 
-=item C<arn:aws:lambda:ap-south-1:445285296882:layer:perl-5-26-runtime:12>
+=item C<arn:aws:lambda:ap-south-1:445285296882:layer:perl-5-26-runtime:13>
 
-=item C<arn:aws:lambda:ap-southeast-1:445285296882:layer:perl-5-26-runtime:12>
+=item C<arn:aws:lambda:ap-southeast-1:445285296882:layer:perl-5-26-runtime:13>
 
-=item C<arn:aws:lambda:ap-southeast-2:445285296882:layer:perl-5-26-runtime:12>
+=item C<arn:aws:lambda:ap-southeast-2:445285296882:layer:perl-5-26-runtime:13>
 
-=item C<arn:aws:lambda:ca-central-1:445285296882:layer:perl-5-26-runtime:12>
+=item C<arn:aws:lambda:ca-central-1:445285296882:layer:perl-5-26-runtime:13>
 
-=item C<arn:aws:lambda:eu-central-1:445285296882:layer:perl-5-26-runtime:12>
+=item C<arn:aws:lambda:eu-central-1:445285296882:layer:perl-5-26-runtime:13>
 
-=item C<arn:aws:lambda:eu-west-1:445285296882:layer:perl-5-26-runtime:12>
+=item C<arn:aws:lambda:eu-west-1:445285296882:layer:perl-5-26-runtime:13>
 
-=item C<arn:aws:lambda:eu-west-2:445285296882:layer:perl-5-26-runtime:11>
+=item C<arn:aws:lambda:eu-west-2:445285296882:layer:perl-5-26-runtime:12>
 
-=item C<arn:aws:lambda:eu-west-3:445285296882:layer:perl-5-26-runtime:11>
+=item C<arn:aws:lambda:eu-west-3:445285296882:layer:perl-5-26-runtime:12>
 
-=item C<arn:aws:lambda:me-south-1:445285296882:layer:perl-5-26-runtime:2>
+=item C<arn:aws:lambda:me-south-1:445285296882:layer:perl-5-26-runtime:3>
 
-=item C<arn:aws:lambda:sa-east-1:445285296882:layer:perl-5-26-runtime:11>
+=item C<arn:aws:lambda:sa-east-1:445285296882:layer:perl-5-26-runtime:12>
 
-=item C<arn:aws:lambda:us-east-1:445285296882:layer:perl-5-26-runtime:11>
+=item C<arn:aws:lambda:us-east-1:445285296882:layer:perl-5-26-runtime:12>
 
-=item C<arn:aws:lambda:us-east-2:445285296882:layer:perl-5-26-runtime:11>
+=item C<arn:aws:lambda:us-east-2:445285296882:layer:perl-5-26-runtime:12>
 
-=item C<arn:aws:lambda:us-west-1:445285296882:layer:perl-5-26-runtime:11>
+=item C<arn:aws:lambda:us-west-1:445285296882:layer:perl-5-26-runtime:12>
 
-=item C<arn:aws:lambda:us-west-2:445285296882:layer:perl-5-26-runtime:11>
+=item C<arn:aws:lambda:us-west-2:445285296882:layer:perl-5-26-runtime:12>
 
 =back
 
@@ -319,39 +319,39 @@ The Layer ARN list is here.
 
 =over
 
-=item C<arn:aws:lambda:ap-east-1:445285296882:layer:perl-5-30-paws:3>
+=item C<arn:aws:lambda:ap-east-1:445285296882:layer:perl-5-30-paws:4>
 
-=item C<arn:aws:lambda:ap-northeast-1:445285296882:layer:perl-5-30-paws:3>
+=item C<arn:aws:lambda:ap-northeast-1:445285296882:layer:perl-5-30-paws:4>
 
-=item C<arn:aws:lambda:ap-northeast-2:445285296882:layer:perl-5-30-paws:3>
+=item C<arn:aws:lambda:ap-northeast-2:445285296882:layer:perl-5-30-paws:4>
 
-=item C<arn:aws:lambda:ap-south-1:445285296882:layer:perl-5-30-paws:3>
+=item C<arn:aws:lambda:ap-south-1:445285296882:layer:perl-5-30-paws:4>
 
-=item C<arn:aws:lambda:ap-southeast-1:445285296882:layer:perl-5-30-paws:3>
+=item C<arn:aws:lambda:ap-southeast-1:445285296882:layer:perl-5-30-paws:4>
 
-=item C<arn:aws:lambda:ap-southeast-2:445285296882:layer:perl-5-30-paws:3>
+=item C<arn:aws:lambda:ap-southeast-2:445285296882:layer:perl-5-30-paws:4>
 
-=item C<arn:aws:lambda:ca-central-1:445285296882:layer:perl-5-30-paws:3>
+=item C<arn:aws:lambda:ca-central-1:445285296882:layer:perl-5-30-paws:4>
 
-=item C<arn:aws:lambda:eu-central-1:445285296882:layer:perl-5-30-paws:3>
+=item C<arn:aws:lambda:eu-central-1:445285296882:layer:perl-5-30-paws:4>
 
-=item C<arn:aws:lambda:eu-west-1:445285296882:layer:perl-5-30-paws:3>
+=item C<arn:aws:lambda:eu-west-1:445285296882:layer:perl-5-30-paws:4>
 
-=item C<arn:aws:lambda:eu-west-2:445285296882:layer:perl-5-30-paws:3>
+=item C<arn:aws:lambda:eu-west-2:445285296882:layer:perl-5-30-paws:4>
 
-=item C<arn:aws:lambda:eu-west-3:445285296882:layer:perl-5-30-paws:3>
+=item C<arn:aws:lambda:eu-west-3:445285296882:layer:perl-5-30-paws:4>
 
-=item C<arn:aws:lambda:me-south-1:445285296882:layer:perl-5-30-paws:2>
+=item C<arn:aws:lambda:me-south-1:445285296882:layer:perl-5-30-paws:3>
 
-=item C<arn:aws:lambda:sa-east-1:445285296882:layer:perl-5-30-paws:3>
+=item C<arn:aws:lambda:sa-east-1:445285296882:layer:perl-5-30-paws:4>
 
-=item C<arn:aws:lambda:us-east-1:445285296882:layer:perl-5-30-paws:3>
+=item C<arn:aws:lambda:us-east-1:445285296882:layer:perl-5-30-paws:4>
 
-=item C<arn:aws:lambda:us-east-2:445285296882:layer:perl-5-30-paws:3>
+=item C<arn:aws:lambda:us-east-2:445285296882:layer:perl-5-30-paws:4>
 
-=item C<arn:aws:lambda:us-west-1:445285296882:layer:perl-5-30-paws:3>
+=item C<arn:aws:lambda:us-west-1:445285296882:layer:perl-5-30-paws:4>
 
-=item C<arn:aws:lambda:us-west-2:445285296882:layer:perl-5-30-paws:3>
+=item C<arn:aws:lambda:us-west-2:445285296882:layer:perl-5-30-paws:4>
 
 =back
 
@@ -359,39 +359,39 @@ The Layer ARN list is here.
 
 =over
 
-=item C<arn:aws:lambda:ap-east-1:445285296882:layer:perl-5-28-paws:3>
+=item C<arn:aws:lambda:ap-east-1:445285296882:layer:perl-5-28-paws:4>
 
-=item C<arn:aws:lambda:ap-northeast-1:445285296882:layer:perl-5-28-paws:3>
+=item C<arn:aws:lambda:ap-northeast-1:445285296882:layer:perl-5-28-paws:4>
 
-=item C<arn:aws:lambda:ap-northeast-2:445285296882:layer:perl-5-28-paws:3>
+=item C<arn:aws:lambda:ap-northeast-2:445285296882:layer:perl-5-28-paws:4>
 
-=item C<arn:aws:lambda:ap-south-1:445285296882:layer:perl-5-28-paws:3>
+=item C<arn:aws:lambda:ap-south-1:445285296882:layer:perl-5-28-paws:4>
 
-=item C<arn:aws:lambda:ap-southeast-1:445285296882:layer:perl-5-28-paws:3>
+=item C<arn:aws:lambda:ap-southeast-1:445285296882:layer:perl-5-28-paws:4>
 
-=item C<arn:aws:lambda:ap-southeast-2:445285296882:layer:perl-5-28-paws:3>
+=item C<arn:aws:lambda:ap-southeast-2:445285296882:layer:perl-5-28-paws:4>
 
-=item C<arn:aws:lambda:ca-central-1:445285296882:layer:perl-5-28-paws:3>
+=item C<arn:aws:lambda:ca-central-1:445285296882:layer:perl-5-28-paws:4>
 
-=item C<arn:aws:lambda:eu-central-1:445285296882:layer:perl-5-28-paws:3>
+=item C<arn:aws:lambda:eu-central-1:445285296882:layer:perl-5-28-paws:4>
 
-=item C<arn:aws:lambda:eu-west-1:445285296882:layer:perl-5-28-paws:3>
+=item C<arn:aws:lambda:eu-west-1:445285296882:layer:perl-5-28-paws:4>
 
-=item C<arn:aws:lambda:eu-west-2:445285296882:layer:perl-5-28-paws:3>
+=item C<arn:aws:lambda:eu-west-2:445285296882:layer:perl-5-28-paws:4>
 
-=item C<arn:aws:lambda:eu-west-3:445285296882:layer:perl-5-28-paws:3>
+=item C<arn:aws:lambda:eu-west-3:445285296882:layer:perl-5-28-paws:4>
 
-=item C<arn:aws:lambda:me-south-1:445285296882:layer:perl-5-28-paws:2>
+=item C<arn:aws:lambda:me-south-1:445285296882:layer:perl-5-28-paws:3>
 
-=item C<arn:aws:lambda:sa-east-1:445285296882:layer:perl-5-28-paws:3>
+=item C<arn:aws:lambda:sa-east-1:445285296882:layer:perl-5-28-paws:4>
 
-=item C<arn:aws:lambda:us-east-1:445285296882:layer:perl-5-28-paws:3>
+=item C<arn:aws:lambda:us-east-1:445285296882:layer:perl-5-28-paws:4>
 
-=item C<arn:aws:lambda:us-east-2:445285296882:layer:perl-5-28-paws:3>
+=item C<arn:aws:lambda:us-east-2:445285296882:layer:perl-5-28-paws:4>
 
-=item C<arn:aws:lambda:us-west-1:445285296882:layer:perl-5-28-paws:3>
+=item C<arn:aws:lambda:us-west-1:445285296882:layer:perl-5-28-paws:4>
 
-=item C<arn:aws:lambda:us-west-2:445285296882:layer:perl-5-28-paws:3>
+=item C<arn:aws:lambda:us-west-2:445285296882:layer:perl-5-28-paws:4>
 
 =back
 
@@ -399,39 +399,39 @@ The Layer ARN list is here.
 
 =over
 
-=item C<arn:aws:lambda:ap-east-1:445285296882:layer:perl-5-26-paws:4>
+=item C<arn:aws:lambda:ap-east-1:445285296882:layer:perl-5-26-paws:5>
 
-=item C<arn:aws:lambda:ap-northeast-1:445285296882:layer:perl-5-26-paws:4>
+=item C<arn:aws:lambda:ap-northeast-1:445285296882:layer:perl-5-26-paws:5>
 
-=item C<arn:aws:lambda:ap-northeast-2:445285296882:layer:perl-5-26-paws:4>
+=item C<arn:aws:lambda:ap-northeast-2:445285296882:layer:perl-5-26-paws:5>
 
-=item C<arn:aws:lambda:ap-south-1:445285296882:layer:perl-5-26-paws:4>
+=item C<arn:aws:lambda:ap-south-1:445285296882:layer:perl-5-26-paws:5>
 
-=item C<arn:aws:lambda:ap-southeast-1:445285296882:layer:perl-5-26-paws:4>
+=item C<arn:aws:lambda:ap-southeast-1:445285296882:layer:perl-5-26-paws:5>
 
-=item C<arn:aws:lambda:ap-southeast-2:445285296882:layer:perl-5-26-paws:4>
+=item C<arn:aws:lambda:ap-southeast-2:445285296882:layer:perl-5-26-paws:5>
 
-=item C<arn:aws:lambda:ca-central-1:445285296882:layer:perl-5-26-paws:4>
+=item C<arn:aws:lambda:ca-central-1:445285296882:layer:perl-5-26-paws:5>
 
-=item C<arn:aws:lambda:eu-central-1:445285296882:layer:perl-5-26-paws:4>
+=item C<arn:aws:lambda:eu-central-1:445285296882:layer:perl-5-26-paws:5>
 
-=item C<arn:aws:lambda:eu-west-1:445285296882:layer:perl-5-26-paws:4>
+=item C<arn:aws:lambda:eu-west-1:445285296882:layer:perl-5-26-paws:5>
 
-=item C<arn:aws:lambda:eu-west-2:445285296882:layer:perl-5-26-paws:3>
+=item C<arn:aws:lambda:eu-west-2:445285296882:layer:perl-5-26-paws:4>
 
-=item C<arn:aws:lambda:eu-west-3:445285296882:layer:perl-5-26-paws:3>
+=item C<arn:aws:lambda:eu-west-3:445285296882:layer:perl-5-26-paws:4>
 
-=item C<arn:aws:lambda:me-south-1:445285296882:layer:perl-5-26-paws:2>
+=item C<arn:aws:lambda:me-south-1:445285296882:layer:perl-5-26-paws:3>
 
-=item C<arn:aws:lambda:sa-east-1:445285296882:layer:perl-5-26-paws:3>
+=item C<arn:aws:lambda:sa-east-1:445285296882:layer:perl-5-26-paws:4>
 
-=item C<arn:aws:lambda:us-east-1:445285296882:layer:perl-5-26-paws:3>
+=item C<arn:aws:lambda:us-east-1:445285296882:layer:perl-5-26-paws:4>
 
-=item C<arn:aws:lambda:us-east-2:445285296882:layer:perl-5-26-paws:3>
+=item C<arn:aws:lambda:us-east-2:445285296882:layer:perl-5-26-paws:4>
 
-=item C<arn:aws:lambda:us-west-1:445285296882:layer:perl-5-26-paws:3>
+=item C<arn:aws:lambda:us-west-1:445285296882:layer:perl-5-26-paws:4>
 
-=item C<arn:aws:lambda:us-west-2:445285296882:layer:perl-5-26-paws:3>
+=item C<arn:aws:lambda:us-west-2:445285296882:layer:perl-5-26-paws:4>
 
 =back
 
