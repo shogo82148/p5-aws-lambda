285bc2dbf7f632dc944d4b57557bdae52e049b80
Ichinose Shogo <shogo82148@gmail.com>
2020-08-14 07:52:34 +0900

    install OpenSSL

--- author/build-perl-al2.sh
+++ author/build-perl-al2.sh
@@ -15,15 +15,34 @@ cp -R /usr/lib64/libcurl.* /opt/lib/
 
 # AWS::Lambda is installed as vendor modules.
 # site_perl is reserved for other AWS Lambda layers.
-PERL_MM_OPT="INSTALLDIRS=vendor"
+PERL_MM_OPT="INSTALLDIRS=vendor CCFLAGS=-I/opt/include LIBS=-L/opt/lib"
 export PERL_MM_OPT
-PERL_MB_OPT="--installdirs=vendor"
+PERL_MB_OPT="--installdirs=vendor --ccflags=-I/opt/include --lddlflags=-L/opt/lib"
 export PERL_MB_OPT
 
 /opt/bin/cpan -T App::cpanminus
 /opt/bin/cpan -T .
 /opt/bin/cpan -T AWS::XRay
 cp script/bootstrap /opt/
+
+# Many Web services are encrypted with SSL/TLS.
+# install SSL/TLS modules for convenience.
+# install OpenSSL
+(
+    OPENSSL_VERSION=1.1.1g
+    cd /tmp
+    curl -sL -o /tmp/openssl.tar.gz "https://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz"
+    echo "ddb04774f1e32f0c49751e21b67216ac87852ceb056b75209af2443400636d46  openssl.tar.gz" | sha256sum -
+    tar zxf openssl.tar.gz
+    cd "/tmp/openssl-$OPENSSL_VERSION"
+    ./config --prefix=/opt
+    make
+    make install_sw
+)
+# install SSL/TLS modules
+/opt/bin/cpan -T Net::SSLeay
+/opt/bin/cpan -T Mozilla::CA
+/opt/bin/cpan -T IO::Socket::SSL
 
 # autodie is included in perl core, but the system perl of the Lambda Runtime lacks it.
 yum install -y perl-autodie
