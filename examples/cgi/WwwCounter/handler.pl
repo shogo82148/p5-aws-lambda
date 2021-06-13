692dcc5f705bb4b76c4220696b01769f24bd4f8d
Ichinose Shogo <shogo82148@gmail.com>
2021-06-13 13:54:51 +0900

    configure WWWCOUNT_DIR in the template

--- examples/cgi/WwwCounter/handler.pl
+++ examples/cgi/WwwCounter/handler.pl
@@ -4,7 +4,7 @@ use strict;
 use lib "$ENV{'LAMBDA_TASK_ROOT'}/local/lib/perl5";
 use AWS::Lambda::PSGI;
 
-$ENV{WWWCOUNT_DIR} = "/tmp";
+$ENV{WWWCOUNT_DIR} ||= "/tmp";
 chdir($ENV{'LAMBDA_TASK_ROOT'});
 my $app = require "$ENV{'LAMBDA_TASK_ROOT'}/app.psgi";
 my $func = AWS::Lambda::PSGI->wrap($app);
