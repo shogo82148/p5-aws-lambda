b280bc878e535f5d797b658a49dedd39ef422574
ICHINOSE Shogo <shogo82148@gmail.com>
2023-11-11 22:03:38 +0900

    introduce Paws runtime for AmazonLinux 2023

---
+++ author/build-paws-al2023.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# helper script for build-paws-layer-al2023.sh
+# you should not run this script directly.
+
+set -uex
+
+TAG=$1
+
+PAWS_VERSION=0.44
+
+# install development packages
+dnf install -y glibc-langpack-en expat-devel
+
+case $(uname -m) in
+  "x86_64")
+    ARCH=x86_64;;
+  "aarch64")
+    ARCH=arm64;;
+  *)
+    echo "unknown architecture: $(uname -m)"
+esac
+
+cd /opt
+unzip "/var/task/.perl-layer/dist/perl-$TAG-runtime-al2023-$ARCH.zip"
+/opt/bin/cpanm --notest --no-man-pages "Paws@$PAWS_VERSION"
+
+# remove pods
+find /opt/lib/perl5/site_perl -type f -a -name '*.pod' -delete
