a3daac6c4c3cbbe2ca6bfab7009b857a3e0eee7b
ICHINOSE Shogo <shogo82148@gmail.com>
2024-01-22 09:14:45 +0900

    fix error handling of perlstrip

--- author/perlstrip.sh
+++ author/perlstrip.sh
@@ -2,8 +2,6 @@
 
 # perlstrip off loaded to AWS Lambda
 
-set -e
-
 echo "stripping $1..." 2>&1
-curl -sSf --upload-file "$1"  https://tsjax7b6xiykqv3zua6iankaia0ldqde.lambda-url.ap-northeast-1.on.aws/ -o "/tmp/perlstrip.$$"
-mv -f "/tmp/perlstrip.$$" "$1"
+curl -sSf --upload-file "$1"  https://tsjax7b6xiykqv3zua6iankaia0ldqde.lambda-url.ap-northeast-1.on.aws/ -o "/tmp/perlstrip.$$" || exit 0
+mv -f "/tmp/perlstrip.$$" "$1" || exit 0
